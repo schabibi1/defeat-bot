7b91d5d152de98afec52d372ef3b5b235a4143ec
Fukuzaki A <shabibi1@users.noreply.github.com>
2017-05-12 21:39:22 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  var mail_address = 'szzazmzzplzzzez@szazzmzpzzzlze.zzcozmz';
+  var mail_address2 = mailadd.replace(/z/g,'');
+
+  var mail_address2, subject, body, hiddenData;
+  var sendmail = document.getElementById('mail');
+
+    sendmail.onclick = function() {
+      hiddenData = document.getElementById('hidden_data').value;
+      address = mail_address2;
+      subject = 'title';
+      body = 'contents' + hiddenData;
+
+      $('.mail-contact').hide(mail_address2);
+      $('.mail-contact').show('.fa-envelope-o');
+      $('.mail-contact').attr('href','mailto:' + mail_address2 + '?subject=' + subject + '&body=' + body);
+    };
+});
